5f8e2dbf7d8bcaea546b4636b974605aa8cb262c
connie <connie.lv@inflightdublin.com>
2015-12-22 11:35:42 +0000

    change log in

--- admin/routes/views/home.js
+++ admin/routes/views/home.js
@@ -4,7 +4,7 @@ exports = module.exports = function(req, res) {
 
 	keystone.render(req, res, 'home', {
 		section: 'home',
-		page: 'main',
+		page: 'home',
 		title: keystone.get('name') || 'Keystone',
 		orphanedLists: keystone.getOrphanedLists()
 	});
